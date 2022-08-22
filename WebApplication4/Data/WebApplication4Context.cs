52fb95ebec8ac124075ad9a600c46084fb9144c2
HP <HP@LAPTOP-REP7KRA8>
2022-08-22 09:49:30 +0530

    Add project files.

---
+++ WebApplication4/Data/WebApplication4Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication4.Models;
+
+namespace WebApplication4.Data
+{
+    public class WebApplication4Context : DbContext
+    {
+        public WebApplication4Context (DbContextOptions<WebApplication4Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication4.Models.book> book { get; set; } = default!;
+
+        public DbSet<WebApplication4.Models.usersaccounts> usersaccounts { get; set; }
+
+        public DbSet<WebApplication4.Models.orders> orders { get; set; }
+        public DbSet<WebApplication4.Models.report> report { get; set; }
+    }
+}
